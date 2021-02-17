e0fb8c4bc9e92315925c52418129b0cc0d000936
Timothee Groleau <timothee.groleau@gmail.com>
2021-02-17 18:53:51 +0800

    Small pages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const ULID = require('ulid')
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,22 +11,28 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+	res.render('intro');
+	// res.redirect(`/${ULID.ulid()}`)
+});
+
+app.get('/login', (req, res) => {
+	res.render('login');
+});
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+	res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+	socket.on('join-room', (roomId, userId) => {
+		socket.join(roomId)
+		socket.to(roomId).broadcast.emit('user-connected', userId)
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
+		socket.on('disconnect', () => {
+			socket.to(roomId).broadcast.emit('user-disconnected', userId)
+		})
+	})
 })
 
 server.listen(PORT)
+mu
