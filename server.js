53ff66246719861eead7fd87e8bbd98ec5f0203d
Timothee Groleau <timothee.groleau@gmail.com>
2021-02-17 21:25:57 +0800

    Step 1 of login

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
-const ULID = require('ulid')
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
+const ULID = require('ulid');
 
 const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-	res.render('intro');
-	// res.redirect(`/${ULID.ulid()}`)
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true },
+  genid: () => ULID.ulid()
+}));
+
+
+app.get('/debug', (req, res) => {
+	res.send(JSON.stringify(req.session));
 });
 
 app.get('/login', (req, res) => {
-	res.render('login');
+	res.render('login', {
+		client_id: process.env.TWITCH_CLIENT_ID,
+		redirect_uri: `${req.protocol}://${req.get('host')}/auth/twitch/callback`,
+		scope: 'user_read_email channel_read_subscriptions'
+	});
+});
+
+app.get('/auth/twitch/callback', (req, res) => {
+	req.query.code
+	req.query.scope
+	req.query.scope
+	// extract query string args and validate
 });
 
 app.get('/:room', (req, res) => {
 	res.render('room', { roomId: req.params.room })
-})
+});
 
 io.on('connection', socket => {
 	socket.on('join-room', (roomId, userId) => {
@@ -32,6 +54,13 @@ io.on('connection', socket => {
 			socket.to(roomId).broadcast.emit('user-disconnected', userId)
 		})
 	})
-})
+});
 
 server.listen(PORT);
+
+https://id.twitch.tv/oauth2/authorize?
+response_type=code
+&redirect_uri=https%3A%2F%2Fmaxoutclub.com%2Fauth%2Ftwitch%2Fcallback
+&scope=user%3Aread%3Aemail%20channel%3Aread%3Asubscriptions
+&client_id=kxu1nt4xee7vz4gsebiwy8cld0lfro
+
