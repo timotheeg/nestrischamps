8bfcf4d9c517b8ea950ba7a523fafaf3a5930a5f
Timothee Groleau <timothee.groleau@gmail.com>
2021-02-21 21:41:02 +0800

    More asset path corrections

--- public/ocr/palettes.js
+++ public/ocr/palettes.js
@@ -3,7 +3,7 @@ const LIST = [
 ];
 
 async function getPalette(name) {
-	const response = await fetch(`./palettes/${name}.json`);
+	const response = await fetch(`/ocr/palettes/${name}.json`);
 	const json = await response.json();
 
 	return json.map(([color1, color2]) => [new Uint8Array(color1), new Uint8Array(color2)]);
