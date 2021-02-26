274e3c9b070178072459795acd604639a9672d6e
Timothee Groleau <timothee.groleau@gmail.com>
2021-02-27 00:19:44 +0800

    Add layouts module

---
+++ modules/layouts.js
@@ -0,0 +1,25 @@
+module.exports = {
+	// single player
+	das_trainer: {
+		file: 'skin_das_trainer',
+		type: '1p',
+	},
+	simple_1p: {
+		file: 'simple_1p_layout',
+		type: '1p',
+	},
+	invisible_tetris: {
+		file: 'invisible_tetris',
+		type: '1p',
+	},
+
+	// multi player
+	ctwc: {
+		file: 'competition_layout1',
+		type: 'mp',
+	},
+	ctjc: {
+		file: 'competition_layout1_ctjc',
+		type: 'mp',
+	},
+};
