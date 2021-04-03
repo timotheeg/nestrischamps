20d2236c399798204da4baf66b0154e87b188959
Timothee Groleau <timothee.groleau@gmail.com>
2021-04-04 00:42:50 +0800

    Set up DB in local mode

--- modules/db.js
+++ modules/db.js
@@ -1,6 +1,6 @@
-if (process.env.IS_PUBLIC_SERVER) {
-	const { Pool } = require('pg');
+const { Pool } = require('pg');
 
+if (process.env.IS_PUBLIC_SERVER) {
 	const pool = new Pool({
 		connectionString: process.env.DATABASE_URL,
 		ssl: {
@@ -17,6 +17,7 @@ if (process.env.IS_PUBLIC_SERVER) {
 	module.exports = pool;
 }
 else {
+	/*
 	// Fake Pool for local access
 	// TODO: make a sqlite version
 
@@ -39,4 +40,18 @@ else {
 
 		query,
 	};
+	/**/
+
+	const pool = new Pool({
+		connectionString: process.env.DATABASE_URL
+	});
+
+	// the pool will emit an error on behalf of any idle clients
+	// it contains if a backend error or network partition happens
+	pool.on('error', (err, client) => {
+		console.error('DB: Unexpected error on idle client', err);
+	});
+
+	module.exports = pool;
+
 }
