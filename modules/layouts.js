d57624e5936fee843f115bcdf55cc6b033badd00
Timothee Groleau <tgroleau@zendesk.com>
2021-06-10 16:38:58 +0800

    Generate layout map from filesystem

--- modules/layouts.js
+++ modules/layouts.js
@@ -1,69 +1,21 @@
-module.exports = {
-	// single player
-	classic: {
-		file: 'skin_classic_tetris',
-		type: '1p',
-	},
-	das_trainer: {
-		file: 'skin_das_trainer',
-		type: '1p',
-	},
-	simple_1p: {
-		file: 'simple_1p_layout',
-		type: '1p',
-	},
-	invisible_tetris: {
-		file: 'simple_1p_layout_invisible_tetris',
-		type: '1p',
-	},
-	qualifier: {
-		file: 'qualifier',
-		type: '1p',
-	},
-	stencil: {
-		file: 'stencil',
-		type: '1p',
-	},
-	stencilplus: {
-		file: 'stencilplus',
-		type: '1p',
-	},
-	plaintext: {
-		file: 'plaintext',
-		type: '1p',
-	},
-	champions: {
-		file: 'champions',
-		type: '1p',
-	},
-	tomellosoulman: {
-		file: 'tomellosoulman',
-		type: '1p',
-	},
+const glob = require('glob');
 
-	// multi player
-	ctwc: {
-		file: 'competition_layout1',
-		type: 'mp',
-	},
-	ctjc: {
-		file: 'competition_layout1_ctjc',
-		type: 'mp',
-	},
-	ctjc_pace: {
-		file: 'competition_layout1_ctjc_pace',
-		type: 'mp',
-	},
-	ctm: {
-		file: 'competition_ctm',
-		type: 'mp',
-	},
-	ctm_video: {
-		file: 'competition_ctm_video',
-		type: 'mp',
-	},
-	ctm_video2: {
-		file: 'competition_ctm_video2',
-		type: 'mp',
-	},
-};
+const layouts = {};
+
+['1p', 'mp'].forEach(type => {
+	glob(`public/views/${type}/*.html`, (err, files) => {
+		if (err) {
+			console.error(err);
+			// terminate server
+			return;
+		}
+
+		files.forEach(filename => {
+			const file = filename.split(/[\\/]/).pop().split('.')[0];
+
+			layouts[file] = { file, type };
+		});
+	});
+});
+
+module.exports = layouts;
