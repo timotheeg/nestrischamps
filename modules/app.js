a9cbc852aa44944a9e3dcc950c2193e04245b24d
Timothee Groleau <timothee.groleau@gmail.com>
2021-08-09 15:57:31 +0800

    Log in to Twitch in just one step

--- modules/app.js
+++ modules/app.js
@@ -8,8 +8,39 @@ app.set('trust proxy', 1); // trust first proxy (i.e. heroku) -- needed to get r
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(middlewares.sessionMiddleware);
+
+const TWITCH_LOGIN_BASE_URI = 'https://id.twitch.tv/oauth2/authorize?';
+const TWITCH_LOGIN_QS = new URLSearchParams({
+	client_id: process.env.TWITCH_CLIENT_ID,
+	scope: 'chat:read',
+	response_type: 'code',
+	force_verify: true,
+});
+
+// set up some reusable template data
 app.use((req, res, next) => {
 	res.locals.user = req.session.user;
+
+	if (!req.session.user) {
+		// Since there's no session use
+		// We prep for when user might login
+
+		if (req.originalUrl) {
+			if (!req.originalUrl.startsWith('/auth/')) {
+				req.session.auth_success_redirect = req.originalUrl;
+			}
+		}
+
+		TWITCH_LOGIN_QS.set(
+			'redirect_uri',
+			`${req.protocol}://${req.get('host')}/auth/twitch/callback`
+		);
+
+		const qs = TWITCH_LOGIN_QS.toString();
+
+		res.locals.twitch_login_url = `${TWITCH_LOGIN_BASE_URI}${qs}`;
+	}
+
 	next();
 });
 
