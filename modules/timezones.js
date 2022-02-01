ecc9c1bb5777713c3d603ed54890a6e9f26f7f83
Timothee Groleau <timothee.groleau@gmail.com>
2022-02-01 23:08:45 +0800

    fix: Add missing files modules/timezones

---
+++ modules/timezones.js
@@ -0,0 +1,28 @@
+const timezones = require('timezones.json').reduce((acc, o) => {
+	return acc.push(...o.utc), acc;
+}, []);
+
+const timezones_extras = [
+	'UTC',
+	'US/Alaska',
+	'US/Pacific',
+	'US/Central',
+	'US/Indiana-Starke',
+	'US/Arizona',
+	'US/East-Indiana',
+	'US/Eastern',
+	'US/Hawaii',
+	'US/Aleutian',
+	'US/Michigan',
+	'US/Mountain',
+	'US/Samoa',
+];
+
+timezones.filter(tz => !tz.startsWith('Etc/')).push(...timezones_extras);
+
+const tz_set = new Set(timezones); // to remove duplicates
+
+// Here's hoping the resultant set is a strict subset of Postgres:
+// "SELECT name FROM pg_timezone_names;"
+
+module.exports = [...tz_set].sort();
