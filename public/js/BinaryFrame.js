c1a15b612d2c6b571b6a202cbfeace3ad15af13c
Timothee Groleau <timothee.groleau@gmail.com>
2022-03-01 22:20:26 +0800

    Make BinaryFramea proper module (no need for local privates)

--- public/js/BinaryFrame.js
+++ public/js/BinaryFrame.js
@@ -1,329 +1,323 @@
-const BinaryFrame = (function () {
-	const FORMAT_VERSION = 2;
+const FORMAT_VERSION = 2;
 
-	const FRAME_SIZE_BY_VERSION = {
-		1: 71,
-		2: 72,
-	};
+const FRAME_SIZE_BY_VERSION = {
+	1: 71,
+	2: 72,
+};
 
-	const GAME_TYPE = {
-		CLASSIC: 1,
-		DAS_TRAINER: 2,
-	};
+const GAME_TYPE = {
+	CLASSIC: 1,
+	DAS_TRAINER: 2,
+};
 
-	const PIECE_TO_VALUE = {
-		T: 0,
-		J: 1,
-		Z: 2,
-		O: 3,
-		S: 4,
-		L: 5,
-		I: 6,
-	};
+const PIECE_TO_VALUE = {
+	T: 0,
+	J: 1,
+	Z: 2,
+	O: 3,
+	S: 4,
+	L: 5,
+	I: 6,
+};
 
-	const VALUE_TO_PIECE = {
-		0: 'T',
-		1: 'J',
-		2: 'Z',
-		3: 'O',
-		4: 'S',
-		5: 'L',
-		6: 'I',
-	};
+const VALUE_TO_PIECE = {
+	0: 'T',
+	1: 'J',
+	2: 'Z',
+	3: 'O',
+	4: 'S',
+	5: 'L',
+	6: 'I',
+};
 
-	const PIECES = Object.keys(PIECE_TO_VALUE);
+const PIECES = Object.keys(PIECE_TO_VALUE);
 
-	const NULLABLE_FIELDS = [
-		'score',
-		'lines',
-		'level',
-		'instant_das',
-		'cur_piece_das',
-		...PIECES,
-	];
+const NULLABLE_FIELDS = [
+	'score',
+	'lines',
+	'level',
+	'instant_das',
+	'cur_piece_das',
+	...PIECES,
+];
 
-	const POSSIBLE_RIGHT_PADDING = 50 /* field */ + 1; /* null terminator */
+const POSSIBLE_RIGHT_PADDING = 50 /* field */ + 1; /* null terminator */
 
-	class BinaryFrame {
-		static encode(pojo) {
-			// TODO: validate pojo fields
-			// TODO: throw if values are not compatible with format
+export default class BinaryFrame {
+	static encode(pojo) {
+		// TODO: validate pojo fields
+		// TODO: throw if values are not compatible with format
 
-			// sanitize values to represent null
-			const sanitized = { ...pojo };
+		// sanitize values to represent null
+		const sanitized = { ...pojo };
 
-			NULLABLE_FIELDS.forEach(field => {
-				// capture null and undefined on purpose
-				if (sanitized[field] == null) {
-					sanitized[field] = 0xffffffff;
-				}
-			});
-
-			if (sanitized.preview == null) {
-				sanitized.preview = 0xffffffff;
-			} else {
-				sanitized.preview = PIECE_TO_VALUE[sanitized.preview];
+		NULLABLE_FIELDS.forEach(field => {
+			// capture null and undefined on purpose
+			if (sanitized[field] == null) {
+				sanitized[field] = 0xffffffff;
 			}
+		});
 
-			if (sanitized.cur_piece == null) {
-				sanitized.cur_piece = 0xffffffff;
-			} else {
-				sanitized.cur_piece = PIECE_TO_VALUE[sanitized.cur_piece];
-			}
-
-			if (sanitized.field == null) {
-				sanitized.field = Array(200).fill(0);
-			}
-
-			// fields are now clean, encode!
-
-			const buffer = new Uint8Array(FRAME_SIZE_BY_VERSION[FORMAT_VERSION]);
-
-			let bidx = 0; // byte index
-
-			// header
-			buffer[bidx++] =
-				((FORMAT_VERSION & 0b111) << 5) | ((sanitized.game_type & 0b11) << 3);
-
-			// gameid - 16 bits
-			buffer[bidx++] = (sanitized.gameid & 0xff00) >> 8;
-			buffer[bidx++] = (sanitized.gameid & 0x00ff) >> 0;
-
-			// ctime - 28 bits
-			buffer[bidx++] = (sanitized.ctime & 0xff00000) >> 20;
-			buffer[bidx++] = (sanitized.ctime & 0x00ff000) >> 12;
-			buffer[bidx++] = (sanitized.ctime & 0x0000ff0) >> 4;
-			buffer[bidx++] =
-				((sanitized.ctime & 0x0f) << 4) | ((sanitized.lines & 0xf00) >> 8);
-
-			// lines - 12 bits
-			buffer[bidx++] = sanitized.lines & 0xff;
-
-			// level - 8 bits
-			buffer[bidx++] = sanitized.level & 0xff;
-
-			// score - 24 bits
-			buffer[bidx++] = (sanitized.score & 0xff0000) >> 16;
-			buffer[bidx++] = (sanitized.score & 0x00ff00) >> 8;
-			buffer[bidx++] = (sanitized.score & 0x0000ff);
-
-			// instant_das (5) + preview (3)
-			buffer[bidx++] =
-				((sanitized.instant_das & 0b11111) << 3) | (sanitized.preview & 0b111);
-
-			// cur piece das (5) + cur piece (3)
-			buffer[bidx++] =
-				((sanitized.cur_piece_das & 0b11111) << 3) |
-				(sanitized.cur_piece & 0b111);
-
-			// piece stats (9 bits each - does not lign nicely to byte boundaries 😓)
-			buffer[bidx++] = ((sanitized.T & 0b111111110) >> 1);
-			buffer[bidx++] = ((sanitized.T & 0b000000001) << 7) | ((sanitized.J & 0b111111100) >> 2);
-			buffer[bidx++] = ((sanitized.J & 0b000000011) << 6) | ((sanitized.Z & 0b111111000) >> 3);
-			buffer[bidx++] = ((sanitized.Z & 0b000000111) << 5) | ((sanitized.O & 0b111110000) >> 4);
-			buffer[bidx++] = ((sanitized.O & 0b000001111) << 4) | ((sanitized.S & 0b111100000) >> 5);
-			buffer[bidx++] = ((sanitized.S & 0b000011111) << 3) | ((sanitized.L & 0b111000000) >> 6);
-			buffer[bidx++] = ((sanitized.L & 0b000111111) << 2) | ((sanitized.I & 0b110000000) >> 7);
-			buffer[bidx++] = ((sanitized.I & 0b001111111) << 1);
-
-			// field
-			for (
-				let block_idx = 0;
-				block_idx < sanitized.field.length;
-				block_idx += 4
-			) {
-				buffer[bidx++] =
-					((sanitized.field[block_idx + 0] & 0b11) << 6) |
-					((sanitized.field[block_idx + 1] & 0b11) << 4) |
-					((sanitized.field[block_idx + 2] & 0b11) << 2) |
-					((sanitized.field[block_idx + 3] & 0b11) << 0);
-			}
-
-			return buffer;
+		if (sanitized.preview == null) {
+			sanitized.preview = 0xffffffff;
+		} else {
+			sanitized.preview = PIECE_TO_VALUE[sanitized.preview];
 		}
 
-		static parse(buffer_or_uintarray) {
-			const f = BinaryFrame.getFrameFromBuffer(buffer_or_uintarray); // may throw
-
-			const pojo = {};
-
-			let bidx = 0;
-
-			pojo.version = (f[bidx] & 0b11100000) >> 5;
-			pojo.game_type = (f[bidx] & 0b11000) >> 3;
-			pojo.player_num = f[bidx++] & 0b111;
-
-			pojo.gameid = (f[bidx++] << 8) | f[bidx++];
-
-			pojo.ctime =
-				(f[bidx++] << 20) |
-				(f[bidx++] << 12) |
-				(f[bidx++] << 4) |
-				((f[bidx] & 0xf0) >> 4);
-
-			if (pojo.version === FORMAT_VERSION) {
-				pojo.lines = ((f[bidx++] & 0x0f) << 8) | f[bidx++];
-
-				pojo.level = f[bidx++];
-
-				pojo.score = (f[bidx++] << 16) | (f[bidx++] << 8) | f[bidx++];
-
-				pojo.instant_das = (f[bidx] & 0b11111000) >> 3;
-				pojo.preview = f[bidx++] & 0b111;
-
-				pojo.cur_piece_das = (f[bidx] & 0b11111000) >> 3;
-				pojo.cur_piece = f[bidx++] & 0b111;
-
-				// piece stats)
-				pojo.T = ((f[bidx++] & 0b11111111) << 1) | ((f[bidx] & 0b10000000) >> 7);
-				pojo.J = ((f[bidx++] & 0b01111111) << 2) | ((f[bidx] & 0b11000000) >> 6);
-				pojo.Z = ((f[bidx++] & 0b00111111) << 3) | ((f[bidx] & 0b11100000) >> 5);
-				pojo.O = ((f[bidx++] & 0b00011111) << 4) | ((f[bidx] & 0b11110000) >> 4);
-				pojo.S = ((f[bidx++] & 0b00001111) << 5) | ((f[bidx] & 0b11111000) >> 3);
-				pojo.L = ((f[bidx++] & 0b00000111) << 6) | ((f[bidx] & 0b11111100) >> 2);
-				pojo.I = ((f[bidx++] & 0b00000011) << 7) | ((f[bidx] & 0b11111110) >> 1);
-
-				bidx++;
-			} else {
-				// can only be version 1 for now, no need to check more
-				pojo.score =
-					((f[bidx++] & 0x0f) << 17) |
-					(f[bidx++] << 9) |
-					(f[bidx++] << 1) |
-					((f[bidx] & 0b10000000) >> 7);
-
-				pojo.lines =
-					((f[bidx++] & 0b01111111) << 2) | ((f[bidx] & 0b11000000) >> 6);
-
-				pojo.level = f[bidx++] & 0b0111111;
-
-				pojo.instant_das = (f[bidx] & 0b11111000) >> 3;
-				pojo.preview = f[bidx++] & 0b111;
-
-				pojo.cur_piece_das = (f[bidx] & 0b11111000) >> 3;
-				pojo.cur_piece = f[bidx++] & 0b111;
-
-				// piece stats
-				pojo.T = f[bidx++];
-				pojo.J = f[bidx++];
-				pojo.Z = f[bidx++];
-				pojo.O = f[bidx++];
-				pojo.S = f[bidx++];
-				pojo.L = f[bidx++];
-				pojo.I = f[bidx++];
-			}
-
-			pojo.field = Array(200);
-
-			for (let idx = 0; idx < 50; idx++, bidx++) {
-				pojo.field[idx * 4 + 0] = (f[bidx] & 0b11000000) >> 6;
-				pojo.field[idx * 4 + 1] = (f[bidx] & 0b00110000) >> 4;
-				pojo.field[idx * 4 + 2] = (f[bidx] & 0b00001100) >> 2;
-				pojo.field[idx * 4 + 3] = (f[bidx] & 0b00000011) >> 0;
-			}
-
-			// we've extracted all the value, now checks for nulls
-
-			if (pojo.version === FORMAT_VERSION) {
-				if (pojo.score === 0xffffff) pojo.score = null;
-				if (pojo.lines === 0xfff) pojo.lines = null;
-				if (pojo.level === 0xff) pojo.level = null;
-
-				PIECES.forEach(piece => {
-					if (pojo[piece] === 0x1ff) {
-						pojo[piece] = null;
-					}
-				});
-			}
-			else {
-				if (pojo.score === 0x1fffff) pojo.score = null;
-				if (pojo.lines === 0b111111111) pojo.lines = null;
-				if (pojo.level === 0b111111) pojo.level = null;
-
-				PIECES.forEach(piece => {
-					if (pojo[piece] === 0xff) {
-						pojo[piece] = null;
-					}
-				});
-			}
-
-			if (pojo.instant_das === 0b11111) pojo.instant_das = null;
-			if (pojo.cur_piece_das === 0b11111) pojo.cur_piece_das = null;
-
-
-			if (pojo.preview === 0b111) {
-				pojo.preview = null;
-			} else {
-				pojo.preview = VALUE_TO_PIECE[pojo.preview];
-			}
-
-			if (pojo.cur_piece === 0b111) {
-				pojo.cur_piece = null;
-			} else {
-				pojo.cur_piece = VALUE_TO_PIECE[pojo.cur_piece];
-			}
-
-			return pojo;
+		if (sanitized.cur_piece == null) {
+			sanitized.cur_piece = 0xffffffff;
+		} else {
+			sanitized.cur_piece = PIECE_TO_VALUE[sanitized.cur_piece];
 		}
 
-		static getCTime(frame_arr) {
-			return (
-				(frame_arr[3] << 20) |
-				(frame_arr[4] << 12) |
-				(frame_arr[5] << 4) |
-				((frame_arr[6] & 0xf0) >> 4)
-			);
+		if (sanitized.field == null) {
+			sanitized.field = Array(200).fill(0);
 		}
 
-		static getFrameFromBuffer(buffer_or_uintarray) {
-			let f;
+		// fields are now clean, encode!
 
-			if (buffer_or_uintarray instanceof Uint8Array) {
-				f = buffer_or_uintarray;
-			} else {
-				f = new Uint8Array(buffer_or_uintarray);
-			}
+		const buffer = new Uint8Array(FRAME_SIZE_BY_VERSION[FORMAT_VERSION]);
 
-			const version = f[0] >> 5; // 3 bits with mask 0b11100000
+		let bidx = 0; // byte index
 
-			if (!FRAME_SIZE_BY_VERSION[version]) {
-				throw new Error('Invalid Frame: Version not supported');
-			}
+		// header
+		buffer[bidx++] =
+			((FORMAT_VERSION & 0b111) << 5) | ((sanitized.game_type & 0b11) << 3);
 
-			const normal_size = FRAME_SIZE_BY_VERSION[version];
+		// gameid - 16 bits
+		buffer[bidx++] = (sanitized.gameid & 0xff00) >> 8;
+		buffer[bidx++] = (sanitized.gameid & 0x00ff) >> 0;
 
-			if (f.length === normal_size) {
-				return f;
-			}
+		// ctime - 28 bits
+		buffer[bidx++] = (sanitized.ctime & 0xff00000) >> 20;
+		buffer[bidx++] = (sanitized.ctime & 0x00ff000) >> 12;
+		buffer[bidx++] = (sanitized.ctime & 0x0000ff0) >> 4;
+		buffer[bidx++] =
+			((sanitized.ctime & 0x0f) << 4) | ((sanitized.lines & 0xf00) >> 8);
 
-			if (f.length > normal_size) {
-				throw new Error('Invalid frame: too long');
-			}
+		// lines - 12 bits
+		buffer[bidx++] = sanitized.lines & 0xff;
 
-			// Minor transport optimization below
-			// The tail of the frame (field) may
-			// be omitted and will pad with zeros to compensate
+		// level - 8 bits
+		buffer[bidx++] = sanitized.level & 0xff;
 
-			if (f.length < normal_size - POSSIBLE_RIGHT_PADDING) {
-				throw new Error('Invalid frame: too short');
-			}
+		// score - 24 bits
+		buffer[bidx++] = (sanitized.score & 0xff0000) >> 16;
+		buffer[bidx++] = (sanitized.score & 0x00ff00) >> 8;
+		buffer[bidx++] = (sanitized.score & 0x0000ff);
 
-			// frame is too short, but paddable
-			// 1. init as all zeros
-			const padded = new Uint8Array(normal_size);
+		// instant_das (5) + preview (3)
+		buffer[bidx++] =
+			((sanitized.instant_das & 0b11111) << 3) | (sanitized.preview & 0b111);
 
-			// 2. (inefficient) copy the values from f
-			// Can't we do a fast low-level buffer copy? :'(
-			f.forEach((v, i) => (padded[i] = v));
+		// cur piece das (5) + cur piece (3)
+		buffer[bidx++] =
+			((sanitized.cur_piece_das & 0b11111) << 3) |
+			(sanitized.cur_piece & 0b111);
 
-			return padded;
+		// piece stats (9 bits each - does not lign nicely to byte boundaries 😓)
+		buffer[bidx++] = ((sanitized.T & 0b111111110) >> 1);
+		buffer[bidx++] = ((sanitized.T & 0b000000001) << 7) | ((sanitized.J & 0b111111100) >> 2);
+		buffer[bidx++] = ((sanitized.J & 0b000000011) << 6) | ((sanitized.Z & 0b111111000) >> 3);
+		buffer[bidx++] = ((sanitized.Z & 0b000000111) << 5) | ((sanitized.O & 0b111110000) >> 4);
+		buffer[bidx++] = ((sanitized.O & 0b000001111) << 4) | ((sanitized.S & 0b111100000) >> 5);
+		buffer[bidx++] = ((sanitized.S & 0b000011111) << 3) | ((sanitized.L & 0b111000000) >> 6);
+		buffer[bidx++] = ((sanitized.L & 0b000111111) << 2) | ((sanitized.I & 0b110000000) >> 7);
+		buffer[bidx++] = ((sanitized.I & 0b001111111) << 1);
+
+		// field
+		for (
+			let block_idx = 0;
+			block_idx < sanitized.field.length;
+			block_idx += 4
+		) {
+			buffer[bidx++] =
+				((sanitized.field[block_idx + 0] & 0b11) << 6) |
+				((sanitized.field[block_idx + 1] & 0b11) << 4) |
+				((sanitized.field[block_idx + 2] & 0b11) << 2) |
+				((sanitized.field[block_idx + 3] & 0b11) << 0);
 		}
+
+		return buffer;
 	}
 
-	BinaryFrame.GAME_TYPE = GAME_TYPE;
-	BinaryFrame.FRAME_SIZE_BY_VERSION = FRAME_SIZE_BY_VERSION;
+	static parse(buffer_or_uintarray) {
+		const f = BinaryFrame.getFrameFromBuffer(buffer_or_uintarray); // may throw
 
-	return BinaryFrame;
-})();
+		const pojo = {};
 
-export default BinaryFrame;
+		let bidx = 0;
+
+		pojo.version = (f[bidx] & 0b11100000) >> 5;
+		pojo.game_type = (f[bidx] & 0b11000) >> 3;
+		pojo.player_num = f[bidx++] & 0b111;
+
+		pojo.gameid = (f[bidx++] << 8) | f[bidx++];
+
+		pojo.ctime =
+			(f[bidx++] << 20) |
+			(f[bidx++] << 12) |
+			(f[bidx++] << 4) |
+			((f[bidx] & 0xf0) >> 4);
+
+		if (pojo.version === FORMAT_VERSION) {
+			pojo.lines = ((f[bidx++] & 0x0f) << 8) | f[bidx++];
+
+			pojo.level = f[bidx++];
+
+			pojo.score = (f[bidx++] << 16) | (f[bidx++] << 8) | f[bidx++];
+
+			pojo.instant_das = (f[bidx] & 0b11111000) >> 3;
+			pojo.preview = f[bidx++] & 0b111;
+
+			pojo.cur_piece_das = (f[bidx] & 0b11111000) >> 3;
+			pojo.cur_piece = f[bidx++] & 0b111;
+
+			// piece stats)
+			pojo.T = ((f[bidx++] & 0b11111111) << 1) | ((f[bidx] & 0b10000000) >> 7);
+			pojo.J = ((f[bidx++] & 0b01111111) << 2) | ((f[bidx] & 0b11000000) >> 6);
+			pojo.Z = ((f[bidx++] & 0b00111111) << 3) | ((f[bidx] & 0b11100000) >> 5);
+			pojo.O = ((f[bidx++] & 0b00011111) << 4) | ((f[bidx] & 0b11110000) >> 4);
+			pojo.S = ((f[bidx++] & 0b00001111) << 5) | ((f[bidx] & 0b11111000) >> 3);
+			pojo.L = ((f[bidx++] & 0b00000111) << 6) | ((f[bidx] & 0b11111100) >> 2);
+			pojo.I = ((f[bidx++] & 0b00000011) << 7) | ((f[bidx] & 0b11111110) >> 1);
+
+			bidx++;
+		} else {
+			// can only be version 1 for now, no need to check more
+			pojo.score =
+				((f[bidx++] & 0x0f) << 17) |
+				(f[bidx++] << 9) |
+				(f[bidx++] << 1) |
+				((f[bidx] & 0b10000000) >> 7);
+
+			pojo.lines =
+				((f[bidx++] & 0b01111111) << 2) | ((f[bidx] & 0b11000000) >> 6);
+
+			pojo.level = f[bidx++] & 0b0111111;
+
+			pojo.instant_das = (f[bidx] & 0b11111000) >> 3;
+			pojo.preview = f[bidx++] & 0b111;
+
+			pojo.cur_piece_das = (f[bidx] & 0b11111000) >> 3;
+			pojo.cur_piece = f[bidx++] & 0b111;
+
+			// piece stats
+			pojo.T = f[bidx++];
+			pojo.J = f[bidx++];
+			pojo.Z = f[bidx++];
+			pojo.O = f[bidx++];
+			pojo.S = f[bidx++];
+			pojo.L = f[bidx++];
+			pojo.I = f[bidx++];
+		}
+
+		pojo.field = Array(200);
+
+		for (let idx = 0; idx < 50; idx++, bidx++) {
+			pojo.field[idx * 4 + 0] = (f[bidx] & 0b11000000) >> 6;
+			pojo.field[idx * 4 + 1] = (f[bidx] & 0b00110000) >> 4;
+			pojo.field[idx * 4 + 2] = (f[bidx] & 0b00001100) >> 2;
+			pojo.field[idx * 4 + 3] = (f[bidx] & 0b00000011) >> 0;
+		}
+
+		// we've extracted all the value, now checks for nulls
+
+		if (pojo.version === FORMAT_VERSION) {
+			if (pojo.score === 0xffffff) pojo.score = null;
+			if (pojo.lines === 0xfff) pojo.lines = null;
+			if (pojo.level === 0xff) pojo.level = null;
+
+			PIECES.forEach(piece => {
+				if (pojo[piece] === 0x1ff) {
+					pojo[piece] = null;
+				}
+			});
+		}
+		else {
+			if (pojo.score === 0x1fffff) pojo.score = null;
+			if (pojo.lines === 0b111111111) pojo.lines = null;
+			if (pojo.level === 0b111111) pojo.level = null;
+
+			PIECES.forEach(piece => {
+				if (pojo[piece] === 0xff) {
+					pojo[piece] = null;
+				}
+			});
+		}
+
+		if (pojo.instant_das === 0b11111) pojo.instant_das = null;
+		if (pojo.cur_piece_das === 0b11111) pojo.cur_piece_das = null;
+
+
+		if (pojo.preview === 0b111) {
+			pojo.preview = null;
+		} else {
+			pojo.preview = VALUE_TO_PIECE[pojo.preview];
+		}
+
+		if (pojo.cur_piece === 0b111) {
+			pojo.cur_piece = null;
+		} else {
+			pojo.cur_piece = VALUE_TO_PIECE[pojo.cur_piece];
+		}
+
+		return pojo;
+	}
+
+	static getCTime(frame_arr) {
+		return (
+			(frame_arr[3] << 20) |
+			(frame_arr[4] << 12) |
+			(frame_arr[5] << 4) |
+			((frame_arr[6] & 0xf0) >> 4)
+		);
+	}
+
+	static getFrameFromBuffer(buffer_or_uintarray) {
+		let f;
+
+		if (buffer_or_uintarray instanceof Uint8Array) {
+			f = buffer_or_uintarray;
+		} else {
+			f = new Uint8Array(buffer_or_uintarray);
+		}
+
+		const version = f[0] >> 5; // 3 bits with mask 0b11100000
+
+		if (!FRAME_SIZE_BY_VERSION[version]) {
+			throw new Error('Invalid Frame: Version not supported');
+		}
+
+		const normal_size = FRAME_SIZE_BY_VERSION[version];
+
+		if (f.length === normal_size) {
+			return f;
+		}
+
+		if (f.length > normal_size) {
+			throw new Error('Invalid frame: too long');
+		}
+
+		// Minor transport optimization below
+		// The tail of the frame (field) may
+		// be omitted and will pad with zeros to compensate
+
+		if (f.length < normal_size - POSSIBLE_RIGHT_PADDING) {
+			throw new Error('Invalid frame: too short');
+		}
+
+		// frame is too short, but paddable
+		// 1. init as all zeros
+		const padded = new Uint8Array(normal_size);
+
+		// 2. (inefficient) copy the values from f
+		// Can't we do a fast low-level buffer copy? :'(
+		f.forEach((v, i) => (padded[i] = v));
+
+		return padded;
+	}
+}
+
+BinaryFrame.GAME_TYPE = GAME_TYPE;
+BinaryFrame.FRAME_SIZE_BY_VERSION = FRAME_SIZE_BY_VERSION;
