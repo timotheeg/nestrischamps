7402dc3a7c610108706cd88cd201e0c7ea9d8789
Timothee Groleau <timothee.groleau@gmail.com>
2022-03-26 08:13:45 +0800

    Positionner done

--- public/views/addStackRabbitRecommendation.js
+++ public/views/addStackRabbitRecommendation.js
@@ -1,5 +1,8 @@
 // represent the spawn point at top of screen
-const spawn_and_rotations = {
+const OFFSET_X = 3;
+const OFFSET_Y = -2;
+
+const ROTATIONS = {
 	T: [
 		[
 			[0, 0, 0, 0],
@@ -131,4 +134,35 @@ const spawn_and_rotations = {
 };
 
 // from stack rabbit evaluation, compute where the piece's final position is
-export default function addStackRabbitRecommendation(field, piece, placement) {}
+export default function addStackRabbitRecommendation(field, piece, placement) {
+	const [rotation, xshift, yshift] = placement;
+	const field_copy = [...field];
+	const shape = ROTATIONS[piece][rotation];
+	const offset_x = OFFSET_X + xshift;
+	const offset_y = OFFSET_Y + yshift;
+
+	// put piece in field, warn if error (piece taking non-empty space)
+
+	for (let x = 0; x < 4; x++) {
+		for (let y = 0; y < 4; y++) {
+			const block = shape[y][x];
+			if (!block) continue;
+			const target_x = offset_x + x;
+			if (target_x < 0 || target_x >= 10) continue;
+			const target_y = offset_y + y;
+			if (target_y < 0 || target_y >= 19) continue;
+
+			const target_idx = target_y * 10 + target_x;
+
+			if (field_copy[target_idx]) {
+				console.warn(
+					`Stack Rabbit pLacement collides with board block at ${target_x}x${target_y}`
+				);
+			}
+
+			field_copy[target_idx] = block;
+		}
+	}
+
+	return field_copy;
+}
