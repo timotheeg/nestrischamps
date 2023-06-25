3d36977f441990b84ef775499e0d04322e627b36
Timothee Groleau <timothee.groleau@gmail.com>
2023-06-25 16:23:12 +0800

    feat: allow local logins up to 16 players

---
+++ setup/20230625.sql
@@ -0,0 +1,15 @@
+INSERT INTO twitch_users
+(id, login, email, secret, type, description, display_name, profile_image_url, dob, country_code, city, interests, style, timezone, created_on, last_login)
+VALUES
+(5,  'player5',  'player5@nestrischamps.io',  'PLAYER5',  '', '', 'Player 5',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(6,  'player6',  'player6@nestrischamps.io',  'PLAYER6',  '', '', 'Player 6',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(7,  'player7',  'player7@nestrischamps.io',  'PLAYER7',  '', '', 'Player 7',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(8,  'player8',  'player8@nestrischamps.io',  'PLAYER8',  '', '', 'Player 8',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(9,  'player9',  'player9@nestrischamps.io',  'PLAYER9',  '', '', 'Player 9',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(10, 'player10', 'player10@nestrischamps.io', 'PLAYER10', '', '', 'Player 10', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(11, 'player11', 'player11@nestrischamps.io', 'PLAYER11', '', '', 'Player 11', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(12, 'player12', 'player12@nestrischamps.io', 'PLAYER12', '', '', 'Player 12', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(13, 'player13', 'player13@nestrischamps.io', 'PLAYER13', '', '', 'Player 13', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(14, 'player14', 'player14@nestrischamps.io', 'PLAYER14', '', '', 'Player 14', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(15, 'player15', 'player15@nestrischamps.io', 'PLAYER15', '', '', 'Player 15', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(16, 'player16', 'player16@nestrischamps.io', 'PLAYER16', '', '', 'Player 16', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW());
--- setup/db.sql
+++ setup/db.sql
@@ -71,8 +71,23 @@ ALTER TABLE sessions ADD CONSTRAINT session_pkey PRIMARY KEY (sid) NOT DEFERRABL
 
 CREATE INDEX IDX_session_expire ON sessions (expire);
 
-INSERT INTO twitch_users VALUES (1, 'player1', 'player1@nestrischamps.io', 'PLAYER1', '', '', 'Player 1', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW());
-INSERT INTO twitch_users VALUES (2, 'player2', 'player2@nestrischamps.io', 'PLAYER2', '', '', 'Player 2', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW());
-INSERT INTO twitch_users VALUES (3, 'player3', 'player3@nestrischamps.io', 'PLAYER3', '', '', 'Player 3', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW());
-INSERT INTO twitch_users VALUES (4, 'player4', 'player4@nestrischamps.io', 'PLAYER4', '', '', 'Player 4', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW());
 
+INSERT INTO twitch_users
+(id, login, email, secret, type, description, display_name, profile_image_url, dob, country_code, city, interests, style, timezone, created_on, last_login)
+VALUES
+(1,  'player1',  'player1@nestrischamps.io',  'PLAYER1',  '', '', 'Player 1',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(2,  'player2',  'player2@nestrischamps.io',  'PLAYER2',  '', '', 'Player 2',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(3,  'player3',  'player3@nestrischamps.io',  'PLAYER3',  '', '', 'Player 3',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(4,  'player4',  'player4@nestrischamps.io',  'PLAYER4',  '', '', 'Player 4',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(5,  'player5',  'player5@nestrischamps.io',  'PLAYER5',  '', '', 'Player 5',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(6,  'player6',  'player6@nestrischamps.io',  'PLAYER6',  '', '', 'Player 6',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(7,  'player7',  'player7@nestrischamps.io',  'PLAYER7',  '', '', 'Player 7',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(8,  'player8',  'player8@nestrischamps.io',  'PLAYER8',  '', '', 'Player 8',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(9,  'player9',  'player9@nestrischamps.io',  'PLAYER9',  '', '', 'Player 9',  '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(10, 'player10', 'player10@nestrischamps.io', 'PLAYER10', '', '', 'Player 10', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(11, 'player11', 'player11@nestrischamps.io', 'PLAYER11', '', '', 'Player 11', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(12, 'player12', 'player12@nestrischamps.io', 'PLAYER12', '', '', 'Player 12', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(13, 'player13', 'player13@nestrischamps.io', 'PLAYER13', '', '', 'Player 13', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(14, 'player14', 'player14@nestrischamps.io', 'PLAYER14', '', '', 'Player 14', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(15, 'player15', 'player15@nestrischamps.io', 'PLAYER15', '', '', 'Player 15', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW()),
+(16, 'player16', 'player16@nestrischamps.io', 'PLAYER16', '', '', 'Player 16', '', NOW(), '', '', '', 'das', 'UTC', NOW(), NOW());
