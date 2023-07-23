1ecbd44abbeff135a9cf4dd6af47b7c323f9f7cf
Timothee Groleau <timothee.groleau@gmail.com>
2023-07-23 22:44:39 +0800

    feat: support both http and https

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 import { WebSocketServer } from 'ws';
-import { Server } from 'http';
 import fs from 'fs';
+import http from 'http';
 import https from 'https';
 
 import app from './modules/app.js';
 
-// const server = Server(app);
-
-const server = https.createServer(
-	{
-		key: fs.readFileSync('key.pem'),
-		cert: fs.readFileSync('cert.pem'),
-	},
-	app
-);
+import websocketInitializer from './routes/websocket.js';
 
 const wss = new WebSocketServer({
 	clientTracking: false,
 	noServer: true,
 });
 
-import websocketInitializer from './routes/websocket.js';
+const server = http.createServer(app);
+
+const sslServer = https.createServer(
+	{
+		key: fs.readFileSync('key.pem'),
+		cert: fs.readFileSync('cert.pem'),
+	},
+	app.handle.bind(app)
+);
 
 websocketInitializer(server, wss);
+websocketInitializer(sslServer, wss);
 
-server.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 5080);
+sslServer.listen(process.env.SSL_PORT || 5443);
+
+console.log([process.env.PORT || 5080, process.env.SSL_PORT || 5443]);
