c2a9584f1f2257e94808a61a6640b5d9dba18088
Timothee Groleau <timothee.groleau@gmail.com>
2024-01-31 09:12:33 +0800

    refactor: remove unecessary indentation

--- domains/MatchRoom.js
+++ domains/MatchRoom.js
@@ -690,24 +690,24 @@ class MatchRoom extends Room {
 		let send_count = 0;
 
 		this.state.players.forEach((player, p_idx) => {
-			if (player.id === user.id) {
-				if (message instanceof Uint8Array) {
-					if (send_count++ > 0) {
-						// sendToViews() ultimately relies on socket.write(), which is not synchronous
-						// if the same user is assigned to multiple players in the room, then mutating message will
-						// cause duplicate frames when player_num overwrites the value.
-						// we make a copy to ensure each player gets its own message
-						message = new Uint8Array(message);
-					}
-					message[0] = (message[0] & 0b11111000) | p_idx; // sets player number in header byte of binary message
-					this.sendToViews(message);
-				} else if (Array.isArray(message)) {
-					this.sendToViews([message[0], p_idx, ...message.slice(1)]);
-					// TODO: send message to admin page as well?
-				} else {
-					// assume frame
-					this.sendToViews(['frame', p_idx, message]);
+			if (player.id !== user.id) return;
+
+			if (message instanceof Uint8Array) {
+				if (send_count++ > 0) {
+					// sendToViews() ultimately relies on socket.write(), which is not synchronous
+					// if the same user is assigned to multiple players in the room, then mutating message will
+					// cause duplicate frames when player_num overwrites the value.
+					// we make a copy to ensure each player gets its own message
+					message = new Uint8Array(message);
 				}
+				message[0] = (message[0] & 0b11111000) | p_idx; // sets player number in header byte of binary message
+				this.sendToViews(message);
+			} else if (Array.isArray(message)) {
+				this.sendToViews([message[0], p_idx, ...message.slice(1)]);
+				// TODO: send message to admin page as well?
+			} else {
+				// assume frame
+				this.sendToViews(['frame', p_idx, message]);
 			}
 		});
 	}
