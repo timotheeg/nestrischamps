49f1ca984f97d40384cec69f3df1e072ad0b8c59
Timothee Groleau <timothee.groleau@gmail.com>
2024-02-18 15:49:46 +0800

    feat: store gameid in local storage to survive refreshes

--- public/ocr/GameTracker.js
+++ public/ocr/GameTracker.js
@@ -67,15 +67,30 @@ export default class GameTracker {
 	}
 
 	getNextGameId() {
+		let new_game_id = 0;
+
 		// we want to minimize the risk of gameid being duplicated when the producer page refreshes
-		let new_game_id =
-			this.gameid === undefined
-				? Date.now() + Math.floor(Math.random() * (1 << 30))
-				: this.gameid + 1;
+		if (this.gameid === undefined) {
+			new_game_id = localStorage.getItem('gameid');
+
+			if (new_game_id !== null) {
+				new_game_id = parseInt(new_game_id) + 1; // could be NaN if local storage value is garbage (shouldn't happen, just being paranoid)
+			}
+
+			if (new_game_id === null || isNaN(new_game_id)) {
+				new_game_id = Date.now() + Math.floor(Math.random() * (1 << 30));
+			}
+		} else {
+			new_game_id = this.gameid + 1;
+		}
 
 		new_game_id %= 0xffff; // because gameid in binary frame format is 16 bits
 
-		return new_game_id || 1; // never report gameid as 0, just because 🤷‍♂️
+		if (!new_game_id) new_game_id = 1; // never report gameid as 0, so we can always assume a valid gameid is truthy
+
+		localStorage.setItem('gameid', new_game_id);
+
+		return new_game_id;
 	}
 
 	async processFrame(bitmap, half_height) {
