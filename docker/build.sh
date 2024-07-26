7e5544269d4180a0714dbf6e1cde134bef1d27c4
zohassadar <zohassadar@gmail.com>
2024-07-26 18:01:37 +0000

    feat: HTTPS option in docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-docker-compose build nc
+
+if [[ -n $HTTPS ]]; then
+    echo "Generating self signed cert"
+    openssl req \
+        -x509 \
+        -newkey rsa:4096 \
+        -keyout key.pem \
+        -out cert.pem \
+        -sha256 \
+        -days 3650 \
+        -nodes \
+        -subj "/C=XX/ST=State/L=City/O=NTC/OU=NTC/CN=localhost"
+    mv *.pem nestrischamps/
+fi
+
+docker-compose build nc --build-arg HTTPS=$HTTPS
+
+if [[ -n $HTTPS ]]; then
+    rm nestrischamps/*.pem
+fi
+
 docker create -ti --name cpsql nestrischamps bash
 docker cp cpsql:/nestrischamps/setup/db.sql postgresql/
 docker rm -f cpsql
