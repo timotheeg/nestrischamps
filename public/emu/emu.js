4e4eb99d8381af18de03278af0009540fa72f2d8
Timothee Groleau <timothee@open.gov.sg>
2024-08-04 00:07:10 +0800

    Polyfill for FF

--- public/emu/emu.js
+++ public/emu/emu.js
@@ -1,5 +1,38 @@
 import { MarcFile, parseBPSFile } from '/emu/bps.js';
 
+function showOpenFilePickerPolyfill(options) {
+	return new Promise(resolve => {
+		const input = document.createElement('input');
+		input.type = 'file';
+		input.multiple = options.multiple;
+		if (options.types) {
+			input.accept = options.types
+				.map(type => type.accept)
+				.flatMap(inst => Object.keys(inst).flatMap(key => inst[key]))
+				.join(',');
+		}
+
+		input.addEventListener('change', () => {
+			resolve(
+				[...input.files].map(file => {
+					return {
+						getFile: async () =>
+							new Promise(resolve => {
+								resolve(file);
+							}),
+					};
+				})
+			);
+		});
+
+		input.click();
+	});
+}
+
+if (typeof window.showOpenFilePicker !== 'function') {
+	window.showOpenFilePicker = showOpenFilePickerPolyfill;
+}
+
 const first_time = document.querySelector('.first_time');
 const patch_url = '/emu/TetrisGYM-6.0.0.bps';
 
