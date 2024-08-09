77259c1576a2d664ddb5c5c62d76d2117cfdb20a
Timothee Groleau <timothee@open.gov.sg>
2024-08-09 17:11:53 +0800

    fix: showOpenFilePickerPolyfill wnow works in safari

--- public/emu/main.js
+++ public/emu/main.js
@@ -652,6 +652,12 @@ function showOpenFilePickerPolyfill(options) {
 				.join(',');
 		}
 
+		// See https://stackoverflow.com/questions/47664777/javascript-file-input-onchange-not-working-ios-safari-only
+		input.style.position = 'fixed';
+		input.style.top = '-100000px';
+		input.style.left = '-100000px';
+		document.body.appendChild(input);
+
 		input.addEventListener('change', () => {
 			resolve(
 				[...input.files].map(file => {
